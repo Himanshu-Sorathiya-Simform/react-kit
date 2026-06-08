6ece0327ddd6be44858911fd33ce7d466737b607
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-06-08 11:16:21 +0530

    Add: Modal component and css files handling native dialog behavior.

---
+++ src/components/Modal.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+import { useModal } from "../hooks/useModal.ts";
+
+interface ModalProps {
+	children: React.ReactNode;
+	className?: string;
+}
+
+function Modal({ children, className }: ModalProps) {
+	const { id, closeModal } = useModal();
+
+	const dialogRef = useRef<HTMLDialogElement | null>(null);
+
+	useEffect(() => {
+		const dialog = dialogRef.current;
+		if (!dialog) return;
+
+		if (id && !dialog.open) {
+			dialog.showModal();
+		} else if (!id && dialog.open) {
+			dialog.close();
+		}
+	}, [id]);
+
+	const handleCancel = (event: React.SyntheticEvent<HTMLDialogElement>) => {
+		event.preventDefault();
+
+		closeModal();
+	};
+
+	return createPortal(
+		<dialog
+			ref={dialogRef}
+			onCancel={handleCancel}
+			className={`universal-modal ${className}`}
+		>
+			<div className="modal-container">{children}</div>
+		</dialog>,
+		document.body,
+	);
+}
+
+export { Modal };
