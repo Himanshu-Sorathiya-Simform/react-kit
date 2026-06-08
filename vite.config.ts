91748e4c40106e4773aebdbb46c983256c85475d
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-06-08 12:28:31 +0530

    Fix: build generate .d.ts file.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 import pkg from "./package.json";
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		dts({
+			insertTypesEntry: true,
+		}),
+	],
 	build: {
 		lib: {
 			entry: resolve(__dirname, "src/index.ts"),
