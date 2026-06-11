82820879decb82497172f08ea1f4926c4a11053f
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-06-11 12:31:31 +0530

    Release: v1.0.5

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 export { Modal } from "./components/Modal.tsx";
 
+export { useFilter } from "./hooks/useFilter.ts";
 export { useModal } from "./hooks/useModal.ts";
 export { usePagination } from "./hooks/usePagination.ts";
+export { useSort } from "./hooks/useSort.ts";
 
 export { type UseModalReturn } from "./hooks/useModal.ts";
 export { type UsePaginationReturn } from "./hooks/usePagination.ts";
