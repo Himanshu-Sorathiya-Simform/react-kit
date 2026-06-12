f3d0a59de60662e2592a416f60f56da6f8bcac4a
Himanshu Sorathiya <himanshu.sorathiya@simformsolutions.com>
2026-06-12 16:19:53 +0530

    Add: exporting of types of filter and sort hooks

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
-export { Modal } from "./components/Modal.tsx";
+export { ModalLayout } from "./components/Modal.tsx";
 
 export { useFilter } from "./hooks/useFilter.ts";
 export { useModal } from "./hooks/useModal.ts";
 export { usePagination } from "./hooks/usePagination.ts";
 export { useSort } from "./hooks/useSort.ts";
 
+export {
+	type BooleanOperator,
+	type CustomOperator,
+	type DateOperator,
+	type FilterConfig,
+	type FilterOperator,
+	type FilterOptions,
+	type FilterState,
+	type FilterType,
+	type MultiselectOperator,
+	type NumberOperator,
+	type SelectOperator,
+	type TextOperator,
+	type UseFilterReturn,
+} from "./hooks/useFilter.ts";
 export { type UseModalReturn } from "./hooks/useModal.ts";
 export { type UsePaginationReturn } from "./hooks/usePagination.ts";
+export {
+	type SortConfig,
+	type SortOptions,
+	type SortState,
+	type SortType,
+	type UseSortReturn,
+} from "./hooks/useSort.ts";
